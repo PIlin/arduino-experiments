5c41f5f3e38f21c1136f7890ecfdd720130b59cb
Pavel, Ilin <ilin.pa@gmail.com>
2013-01-13 20:03:47 +0100

    Two helper sketches

---
+++ empty/src/sketch.ino
@@ -0,0 +1,20 @@
+#define TYPED_VAR(type, val) ((type)val)
+
+#define ULONG_MAX TYPED_VAR(unsigned long, 0xFFFFFFFF)
+
+void setup()
+{
+	//Serial.begin(9600);
+}
+
+void loop()
+{
+
+	// for (int i = 0; i < 100; ++i)
+	// {
+	// 	Serial.println(millis());
+	// }
+
+	// Serial.println("woop");
+
+}
---
+++ serial_passthrough/src/sketch.ino
@@ -0,0 +1,28 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial sserial(10, 11);
+
+void setup()
+{
+	Serial.begin(9600);
+
+	sserial.begin(9600);
+}
+
+void loop()
+{
+	if (sserial.available())
+	{
+		Serial.print((char)sserial.read());
+	}
+
+
+	if (Serial.available())
+	{
+		char c = Serial.read();
+		sserial.print(c);
+		// Serial.print(c);
+	}
+}
+

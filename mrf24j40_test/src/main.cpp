529b6b31722ea43a0bbea50a1c2fcdb468f1f95a
Pavel Ilin <ilin.pa@gmail.com>
2013-04-22 14:43:48 +0200

    example works

--- mrf24j40_test/src/main.cpp
+++ mrf24j40_test/src/main.cpp
@@ -1,12 +1,17 @@
 
 #include <Arduino.h>
 
+#include <SPI.h>
+
 #include "mrf24j.h"
 
-# define DEBUG_PRINT(x)   do { Serial.print(x);   } while (0)
-# define DEBUG_PRINTLN(x) do { Serial.println(x); } while (0)
-# define DEBUG_PRINTLN2(x,y) do { DEBUG_PRINT(x); DEBUG_PRINTLN(y); } while (0)
-# define DEBUG_PRINTLN2H(x,y) do { DEBUG_PRINT(x); Serial.println(y, HEX); } while (0)
+#define xstr(x) str(x)
+#define str(x) #x
+
+#define DEBUG_PRINT(x)   do { Serial.print(x);   } while (0)
+#define DEBUG_PRINTLN(x) do { Serial.println(x); } while (0)
+#define DEBUG_PRINTLN2(x,y) do { DEBUG_PRINT(x); DEBUG_PRINTLN(y); } while (0)
+#define DEBUG_PRINTLN2H(x,y) do { DEBUG_PRINT(x); Serial.println(y, HEX); } while (0)
 
 #define DEBUG_SHORT(x) do { DEBUG_PRINTLN2H(#x " ", mrf.read_short(x)); } while (0)
 #define DEBUG_LONG(x) do { DEBUG_PRINTLN2H(#x " ", mrf.read_long(x)); } while (0)
@@ -31,8 +36,12 @@ static void blink(uint8_t count, const unsigned long t)
 
 static const int pin_reset = 5;
 // static const int pin_cs = 7;
-static const int pin_cs = 10;
-static const int pin_interrupt = 3;
+static const int pin_cs = 7;
+static const int pin_interrupt = 2;
+
+static word addresses[] = {
+	0x6001, 0x2842
+};
 
 static Mrf24j mrf(pin_reset, pin_cs, pin_interrupt);
 
@@ -44,29 +53,71 @@ static void mrf_interrupt(void)
 static void handle_rx(void)
 {
 	Serial.println("handle_rx");
+
+	Serial.print("received a packet ");
+	Serial.print(mrf.get_rxinfo()->frame_length, DEC);
+	Serial.println(" bytes long");
+
+	if(mrf.get_bufferPHY()){
+		Serial.println("Packet data (PHY Payload):");
+		for (int i = 0; i < mrf.get_rxinfo()->frame_length; i++) {
+			Serial.print(mrf.get_rxbuf()[i]);
+		}
+	}
+
+	Serial.println("\r\nASCII data (relevant data):");
+	for (int i = 0; i < mrf.rx_datalength(); i++) {
+		Serial.write(mrf.get_rxinfo()->rx_data[i]);
+	}
+
+	Serial.print("\r\nLQI/RSSI=");
+	Serial.print(mrf.get_rxinfo()->lqi, DEC);
+	Serial.print("/");
+	Serial.println(mrf.get_rxinfo()->rssi, DEC);
 }
 
 static void handle_tx(void)
 {
 	Serial.println("handle_tx");
+
+	if (mrf.get_txinfo()->tx_ok) {
+		Serial.println("TX went ok, got ack");
+	} else {
+		Serial.print("TX failed after ");
+		Serial.print(mrf.get_txinfo()->retries);
+		Serial.println(" retries\n");
+	}
 }
 
-void setup() {
-	pinMode(ledPin, OUTPUT);
+static void setup_mrf(void)
+{
 
-	Serial.begin(57600);
-	while (!Serial);
 
-	Serial.println("setup mrf");
+	Serial.println("setup mrf node " xstr(ARDUINO_NODE_ID));
+
+	//SPI.setClockDivider(B00000001); // spi speed
 
 	mrf.reset();
 	mrf.init();
 
-	mrf.set_pan(0xabba);
-	mrf.address16_write(0x6001);
+	mrf.set_pan(0x2842abba);
+	mrf.address16_write(addresses[ARDUINO_NODE_ID]);
+
+	//mrf.set_promiscuous(true);
+
+	//mrf.write_short(MRF_RXMCR, 0x02); // error mode
+
+	attachInterrupt(0, mrf_interrupt, CHANGE);
+	interrupts();
+
+	delay(1000);
 
 	Serial.println("done setup mrf");
+}
 
+void check_mrf(void)
+{
+	DEBUG_PRINTLN("check_mrf");
 	int pan = mrf.get_pan();
 	DEBUG_PRINTLN2H("pan ", pan);
 
@@ -87,10 +138,46 @@ void setup() {
 	DEBUG_SHORT(MRF_BBREG2);
 	DEBUG_SHORT(MRF_CCAEDTH);
 	DEBUG_SHORT(MRF_BBREG6);
+
+	DEBUG_PRINTLN("done check_mrf");
+}
+void setup() {
+	pinMode(ledPin, OUTPUT);
+
+	Serial.begin(57600);
+	while (!Serial);
+
+	setup_mrf();
+	check_mrf();
+
 }
 
+static void do_tx()
+{
+	static unsigned long was = millis();
+	unsigned long now = millis();
+	if (now - was >= 1000)
+	{
+		was = now;
+		DEBUG_PRINTLN2("do_tx ", now);
 
+		mrf.send16(addresses[1 - ARDUINO_NODE_ID], "lorem ipsum");
+	}
+
+}
 
 void loop() {
+	static unsigned long was = millis();
+	unsigned long now = millis();
+	if (now - was >= 1000)
+	{
+		was = now;
+		DEBUG_PRINTLN2("alive ", now);
+	}
+
 	mrf.check_flags(&handle_rx, &handle_tx);
+
+#if ARDUINO_NODE_ID == 0
+	do_tx();
+#endif
 }
